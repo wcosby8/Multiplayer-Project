f423b593d7c74f320ae47ea376a9f369429b3cb7
wcosby8 <Wcosby888@gmail.com>
2026-03-05 22:39:23 -0800

    .

--- Assets/Scripts/GridPosition.cs
+++ Assets/Scripts/GridPosition.cs
@@ -2,14 +2,10 @@ using UnityEngine;
 
 public class GridPosition : MonoBehaviour {
 
-
     [SerializeField] private int x;
     [SerializeField] private int y;
-
-
     private void OnMouseDown() {
-        Debug.Log("Click! " + x + ", " + y);
-        GameManager.Instance.ClickedOnGridPositionRpc(x, y, GameManager.Instance.GetLocalPlayerType());
+        Debug.Log($"Click: ({x}, {y})");
+        GameManager.Instance.ClickedOnGridPosition(x, y);
     }
-
 }
